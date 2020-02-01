14f758dd2d49a640949ae185797df150f2985997
Scott Nicholas <scott@nicholas.one>
2020-02-01 21:41:29 +0000

    add echo reply

--- ttl.py
+++ ttl.py
@@ -10,26 +10,38 @@ author:  Scott Nicholas <scott@nicholas.one>
 
 import ctypes
 import struct
-from binascii import hexlify
 from ctypes import create_string_buffer, addressof
 from struct import pack, unpack
 import copy
 import socket
 import array
 
-def ttl2ip(ttl):
+# Ethernet interface to bind to
+INTERFACE = 'ens3'
+
+def ttl2ip(ttl, dest):
   """Define our extra hops here.
 
   TTL 1 is handled by our real interface, let's leave that alone.
   Therefore it starts with ttl-2 index into this list but you can
   do whatever math or list you want.
 
+  Need to cover all TTL in filter and higher should return dest
+  I cover TTL from 2..16 which is 15 hosts, 14 fib numbers and dest itself
+  or TTL >16 if ICMP ECHO REQUEST goes back to send_echo_reply
+
   Returns:
     ip in list format. str should be OK too.
+    None if we should ignore this, TTL is too high
   """
 
-  fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 0, 0, 0, 0]
-  return [44, 38, 10, fib[ttl - 2]]
+  fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+  hop = ttl - 2
+  if hop == len(fib):
+    return dest
+  if hop > len(fib):
+    raise ValueError
+  return [44, 38, 10, fib[hop]]
 
 """The BPF filter code from tcpdump.
 
@@ -38,26 +50,32 @@ make sure you reference correct interface
 whereas my default interface was ip, i will be
 opening my ethernet interface.
 
-tcpdump -i ens3 -dd $expression | tr -s '{}' '()'
-
-ttl < 16 and > 1 and dst host 44.38.10.234
+# tcpdump -p -i ens3 -dd 'ip[8] > 1 and dst host 44.38.10.234 and (icmp[icmptype]==8 or ip[8] < 17)' | tr {} '()'
 """
 filter = [
   ( 0x28, 0, 0, 0x0000000c ),
-  ( 0x15, 0, 6, 0x00000800 ),
+  ( 0x15, 0, 14, 0x00000800 ),
   ( 0x30, 0, 0, 0x00000016 ),
-  ( 0x35, 4, 0, 0x00000010 ),
-  ( 0x25, 0, 3, 0x00000001 ),
+  ( 0x25, 0, 12, 0x00000001 ),
   ( 0x20, 0, 0, 0x0000001e ),
-  ( 0x15, 0, 1, 0x2c260aea ),
+  ( 0x15, 0, 10, 0x2c260aea ),
+  ( 0x30, 0, 0, 0x00000017 ),
+  ( 0x15, 0, 5, 0x00000001 ),
+  ( 0x28, 0, 0, 0x00000014 ),
+  ( 0x45, 3, 0, 0x00001fff ),
+  ( 0xb1, 0, 0, 0x0000000e ),
+  ( 0x50, 0, 0, 0x0000000e ),
+  ( 0x15, 2, 0, 0x00000008 ),
+  ( 0x30, 0, 0, 0x00000016 ),
+  ( 0x35, 1, 0, 0x00000011 ),
   ( 0x6, 0, 0, 0x00040000 ),
   ( 0x6, 0, 0, 0x00000000 ),
 ]
 
 class PHdr(ctypes.BigEndianStructure):
   """I had custom things here but not anymore."""
-
-  pass
+  def __len__(self):
+    return ctypes.sizeof(self)
 
 class ethhdr(PHdr):
   """From include/uapi/linux/if_ether.h."""
@@ -96,7 +114,7 @@ class iphdr(PHdr):
     elif isinstance(ip, list):
       self._saddr = int.from_bytes(ip, 'big')
     else:
-      raise TypeException
+      raise ValueError
 
   @property
   def saddr(self):
@@ -133,27 +151,56 @@ def checksum(pkt):
     return (((s>>8)&0xff)|s<<8) & 0xffff
 
 def send_ttl_expire(s, in_eth, in_ip, payload):
+  """Send ICMP TIME expired from each fake hop."""
   icmp = icmphdr(11, 0, 0, 0, 0)
-
+  try:
+    saddr = ttl2ip(in_ip.ttl, in_ip.daddr)
+  except ValueError:
+    raise ValueError
   ip = copy.copy(in_ip)
-  ip.check = 0
-  ip.tot_len = ctypes.sizeof(ip) + ctypes.sizeof(icmp) + len(payload)
+  ip.protocol = 1
+  ip.tot_len = len(ip) + len(icmp) + len(payload)
   ip.ttl = 63
-  ip.saddr = ttl2ip(in_ip.ttl)
+  ip.saddr = saddr
   ip._daddr = in_ip._saddr
+  ip.check = 0
   ip.check = checksum(bytearray(ip))
 
   eth = ethhdr(in_eth.h_source, in_eth.h_dest, in_eth.h_proto)
 
   icmp.checksum = checksum(bytearray(icmp) + payload)
 
-  msg = create_string_buffer(ctypes.sizeof(eth) + ctypes.sizeof(ip) + ctypes.sizeof(icmp) + len(payload))
+  msg = create_string_buffer(len(eth) + len(ip) + len(icmp) + len(payload))
   msg = bytearray(eth) + bytearray(ip) + bytearray(icmp) + payload
 
   print("  %16s <- %16s ttl:%03d proto:%-3d icmp type:%-3d code:%-3d" % (ip.daddr, ip.saddr, ip.ttl, ip.protocol, icmp.type, icmp.code))
-#  print("send: ", hexlify(msg))
   ret = s.send(msg)
 
+def send_echo_reply(s, in_eth, in_ip, payload):
+  """Send normal ICMP ECHO reply."""
+  icmp = icmphdr.from_buffer_copy(payload)
+  icmp.type = 0
+  icmp.code = 0
+  payload = payload[len(icmp):]
+
+  ip = copy.copy(in_ip)
+  ip.tot_len = len(ip) + len(icmp) + len(payload)
+  ip.ttl = 63
+  ip._saddr = in_ip._daddr
+  ip._daddr = in_ip._saddr
+  ip.check = 0
+  ip.check = checksum(bytearray(ip))
+
+  eth = ethhdr(in_eth.h_source, in_eth.h_dest, in_eth.h_proto)
+
+  icmp.checksum = 0
+  icmp.checksum = checksum(bytearray(icmp) + payload)
+
+  msg = create_string_buffer(len(eth) + len(ip) + len(icmp) + len(payload))
+  msg = bytearray(eth) + bytearray(ip) + bytearray(icmp) + payload
+
+  print("  %16s <- %16s ttl:%03d proto:%-3d icmp type:%-3d code:%-3d" % (ip.daddr, ip.saddr, ip.ttl, ip.protocol, icmp.type, icmp.code))
+  ret = s.send(msg)
 
 def main():
   blob = ctypes.create_string_buffer(b''.join(struct.pack("HBBI", *e) for e in filter))
@@ -165,17 +212,23 @@ def main():
   # Create listening socket with filters
   s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x800))
   s.setsockopt(socket.SOL_SOCKET, SO_ATTACH_FILTER, bpf)
-  s.bind(('ens3', 0x800))
+  s.bind((INTERFACE, 0x800))
   
   while True:
       data, addr = s.recvfrom(65565)
       eth = ethhdr.from_buffer_copy(data)
-      ip = iphdr.from_buffer_copy(data[ctypes.sizeof(ethhdr):])
+      ip = iphdr.from_buffer_copy(data[len(eth):])
 
       # ICMP TIME EXCEED sends back 20 byte header plus first 64-bits of datagram
       print("%s %16s -> %16s ttl:%03d proto:%-3d" % ("*" if ip.ttl == 2 else " ", ip.saddr, ip.daddr, ip.ttl, ip.protocol))
   
-      send_ttl_expire(s, eth, ip, data[14:14+28])
+      try:
+        send_ttl_expire(s, eth, ip, data[14:14+28])
+      except ValueError:
+        """ValueError raised if TTL is too high. Let's actually reply to pings."""
+        p = len(eth) + len(ip)
+        if ip.protocol == 1 and data[p] == 0x08 and data[p+1] == 0x00:
+          send_echo_reply(s, eth, ip, data[p:])
 
 if __name__ == '__main__':
   try:
